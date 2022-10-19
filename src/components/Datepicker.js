76cb17128386d34d7ca34b48b35b58f80f0083c6
java-batista <java.batista@outlook.com>
2022-10-19 16:17:56 -0300

    função para marcar as datas no datepicker

--- src/components/Datepicker.js
+++ src/components/Datepicker.js
@@ -2,17 +2,37 @@ import * as React from 'react';
 import dayjs from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { CalendarPicker } from '@mui/x-date-pickers/CalendarPicker';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import { PickersDay } from '@mui/x-date-pickers';
 
-const minDate = dayjs('2020-01-01T00:00:00.000');
-const maxDate = dayjs('2034-01-01T00:00:00.000');
+const minDate = dayjs('2022-10-01T00:00:00.000');
+const maxDate = dayjs('2022-10-30T00:00:00.000');
+
+const tdate = dayjs('2022-10-15');
 
 const Datepicker = () => {
-  const [date, setDate] = React.useState(dayjs('2022-04-07'));
+  const [date, setDate] = React.useState(dayjs('2022-10-14'));
+
+  const renderDays = (day, selectedDays, pickersDayProps) => {
+
+        let select = day.isSame('2022-10-10', 'day');
+
+    return (
+        <PickersDay {...pickersDayProps} day={day}  selected={select} disabled={!select} /> 
+    );
+  }
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <CalendarPicker date={date} onChange={(newDate) => setDate(newDate)} />
+          <StaticDatePicker 
+            // minDate={minDate}
+            maxDate={maxDate}
+            displayStaticWrapperAs="desktop"
+            value={minDate} 
+            onChange={(newDate) => setDate(newDate)} 
+            renderDay={renderDays}
+            renderInput={(params) => <TextField {...params} />}
+            />
     </LocalizationProvider>
   );
 }
