80437bd5edba4bea50cdc7b89848b41717a84c79
java-batista <java.batista@outlook.com>
2022-10-22 13:57:23 -0300

    layout do modal terminado

--- src/pages/extra-pages/DayDialog.js
+++ src/pages/extra-pages/DayDialog.js
@@ -7,49 +7,116 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
 
+// material-ui
+import { Typography, Grid, TextField } from '@mui/material';
+
+// project import
+import MainCard from 'components/MainCard';
+import StatisticsCard from 'components/cards/statistics/StatisticsCard';
+
 const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
+    return <Slide direction="up" ref={ref} {...props} />;
 });
 
-const DayDialog = ({openDialog, date}) => { 
-  const [open, setOpen] = React.useState(false);
+const DayDialog = ({ openDialog, date }) => {
+    const [open, setOpen] = React.useState(false);
 
-  React.useEffect(() => {
+    React.useEffect(() => {
 
-    if (typeof date != "undefined") {
-        handleClickOpen();
-    } 
-        
-  },[date]);
+        if (typeof date != "undefined") {
+            handleClickOpen();
+        }
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+    }, [date]);
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
 
-  return (
-      <Dialog
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogTitle>{`${date}`}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Voltar</Button>
-        </DialogActions>
-      </Dialog>
-  );
+    const handleClose = () => {
+        setOpen(false);
+    };
+    //{`${date}`}
+
+    return (
+        <Dialog
+            open={open}
+            TransitionComponent={Transition}
+            keepMounted
+            onClose={handleClose}
+            aria-describedby="alert-dialog-slide-description"
+            fullWidth="xl"
+        >
+            <DialogTitle><Typography variant="h5">{`${date}`}</Typography></DialogTitle> 
+            <DialogContent>
+
+                <Grid container rowSpacing={4.5} columnSpacing={2.75}>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <StatisticsCard title="Mês" value={"janeiro"} />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <StatisticsCard title="Total De Dias" value={` 15 ias`} />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <StatisticsCard title="Tempo Total" value={` 1:00:15 horas`} />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <StatisticsCard title="Distância Total" value={`24 Km`} />
+                    </Grid>
+
+                    <Grid item md={8} sx={{ display: { sm: 'none', md: 'block', lg: 'none' } }} />
+
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <StatisticsCard title="Mês" value={"janeiro"} />
+                    </Grid>
+
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <StatisticsCard title="Mês" value={"janeiro"} />
+                    </Grid>
+
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <StatisticsCard title="Mês" value={"janeiro"} />
+                    </Grid>
+
+                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                        <StatisticsCard title="Mês" value={"janeiro"} />
+                    </Grid>
+
+                    <Grid item xs={12} md={7} lg={22}>
+                        <Grid container alignItems="center" justifyContent="space-between">
+                            <Grid item>
+                                <Typography variant="h5">Comentários</Typography>
+                            </Grid>
+                            <Grid item>
+                                <Button
+                                    size="small"
+                                    onClick={() => { }}
+                                    // color={slot === 'distance' ? 'primary' : 'secondary'}
+                                    variant={'text'}
+                                >
+                                    Editar
+                                </Button>
+                            </Grid>
+                        </Grid>
+                        <TextField
+                            id="outlined-multiline-static"
+                            //   label="Multiline"
+                            multiline
+                            rows={4}
+                            defaultValue="Sem comentários"
+                            disabled
+                            margin="normal"
+                            fullWidth
+                        />
+                    </Grid>
+                </Grid>
+
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose} variant='outlined'>Voltar</Button>
+            </DialogActions>
+        </Dialog>
+    );
 };
 
 export default DayDialog;
